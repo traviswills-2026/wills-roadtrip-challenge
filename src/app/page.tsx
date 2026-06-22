5a04319ce6da5d2e0b4a98e81349917986daaea2
Travis Wills <travis.wills@youraspire.com>
2026-06-22 18:11:17 -0400

    Initial project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1> 🚚 Wills Roadtrip Challenge </h1>
+      <p> Welcome to the Wills Roadtrip Challenge! </p>
+      <p> Miles, Memories & Mild Competition </p>
+    </main>
+  )
+}
